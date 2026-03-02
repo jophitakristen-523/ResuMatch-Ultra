1ae0cd15a502e091d12c698b073b17930310beaa
jophitakristen-523 <jophitakristen523@wec.edu.in>
2026-03-02 19:55:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import re
 import random
 import math
+import os
 
 app = Flask(__name__)
 
@@ -583,4 +584,5 @@ def get_countries():
     return jsonify(COUNTRIES_DATA)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
